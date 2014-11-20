540af9905138a2b2aa2ad9bf5b08042cfa9d811e
Administrator <Administrator@WCKIR0YRCMTIMUU>
2014-11-20 13:58:37 +0800

    add Abb.java file

---
+++ src/main/java/com/foton/Abb.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.foton;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Abb {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
